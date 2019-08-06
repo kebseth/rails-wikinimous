1770c0b5a5c7118ab7510cb9854a3075772252e6
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-06 15:57:09 +0200

    avant css

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,46 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
+  end
+
+  def show
+  end
+
+  # CREATE
+  def new
+    @article = Article.new
+  end
+
+  def create
+    article = Article.new(article_params)
+    article.save
+    redirect_to article_path(article)
+  end
+
+  # UPDATE
+
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def edit
+  end
+
+  # DELETE
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
